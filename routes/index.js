3786b5427a6bc2dec1843fe994db5e903200f91f
karasikrus <karasikrus@gmail.com>
2020-03-15 13:21:10 +0300

    meetings with participants are shown

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,49 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-var pgp = require("pg-promise")();
-var dbUrl;
+const pgp = require("pg-promise")();
+let dbUrl;
 try {
-  dbUrl = require('./../config/dbConfig');
+    dbUrl = require('./../config/dbConfig');
 } catch (e) {
-  console.log('no config file');
+    console.log('no config file');
 }
-var db = pgp(dbUrl.postgresUrl);
-
-
+const db = pgp(dbUrl.postgresUrl);
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
-router.get('/meetings', function(req, res, next) {
-  db.any('SELECT * FROM meeting')
-      .then(function(data) {
-        console.log(data);
-        res.send(data);// success;
-      })
-      .catch(function(error) {
-        console.log('error in getting data from database: ', error);// error;
-      });
+router.get('/meetings', function (req, res, next) {
+    db.task(async t => {
+        // t.ctx = task config + state context;
+        let meetings = [];
+        try {
+            meetings = await t.any('SELECT * FROM meeting');
+        } catch (e) {
+            console.log('error in getting meetings from database: ', e);
+        }
+        let promises = [];
+        for (let meeting of meetings) {
+            promises.push(t.any(
+                'SELECT * FROM person WHERE email IN (SELECT person_email FROM participants WHERE meeting_id = $1)',
+                meeting.id))
+        }
+        promises.push(meetings);
+        return Promise.all(promises);
+    })
+        .then(events => {
+            let meetings = events.pop();
+            for (let i = 0; i < meetings.length; i++) {
+                meetings[i].participants = events[i];
+            }
+            res.send(meetings);
+        })
+        .catch(error => {
+            console.log('error in getting participants from database: ', error)// error
+        });
 });
 
 module.exports = router;
